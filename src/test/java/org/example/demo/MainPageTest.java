24d0a09f2b0b8bff8b0039f898eef5e43fb91384
Nutawatnon <nutawatnon@hotmail.com>
2024-07-16 23:50:03 +0700

    Submit The Test

--- src/test/java/org/example/demo/MainPageTest.java
+++ src/test/java/org/example/demo/MainPageTest.java
@@ -1,58 +1,177 @@
 package org.example.demo;
 
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
-import org.openqa.selenium.chrome.ChromeOptions;
-import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.junit.Test;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import static com.codeborne.selenide.Condition.attribute;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.*;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import java.text.DecimalFormat;
+import java.util.List;
+//import org.testng.annotations.Test;
 
 public class MainPageTest {
-    MainPage mainPage = new MainPage();
 
-    @BeforeAll
-    public static void setUpAll() {
-        Configuration.browserSize = "1280x800";
-        SelenideLogger.addListener("allure", new AllureSelenide());
+
+    WebDriver driver;
+    @BeforeTest
+    public void setup() {
+
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
+        driver = new ChromeDriver();
     }
 
-    @BeforeEach
-    public void setUp() {
-        // Fix the issue https://github.com/SeleniumHQ/selenium/issues/11750
-        Configuration.browserCapabilities = new ChromeOptions().addArguments("--remote-allow-origins=*");
-        open("https://www.jetbrains.com/");
-    }
-
-    @Test
-    public void search() {
-        mainPage.searchButton.click();
-
-        $("[data-test='search-input']").sendKeys("Selenium");
-        $("button[data-test='full-search-button']").click();
-
-        $("input[data-test='search-input']").shouldHave(attribute("value", "Selenium"));
+    public void Login_func(){
+        driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+        WebElement username = driver.findElement(By.id("user-name"));
+        WebElement password = driver.findElement(By.id("password"));
+        username.sendKeys("standard_user");
+        password.sendKeys("secret_sauce");
+        WebElement LoginButton = driver.findElement(By.id("login-button"));
+        LoginButton.click();
     }
 
     @Test
-    public void toolsMenu() {
-        mainPage.toolsMenu.click();
+    public void Test1() {
+        //validate login successfully by checking if inventory is displayed.
+        Login_func();
+        WebElement Inventory = driver.findElement(By.id("inventory_container"));
+        assertTrue(Inventory.isDisplayed());
 
-        $("div[data-test='main-submenu']").shouldBe(visible);
     }
 
     @Test
-    public void navigationToAllTools() {
-        mainPage.seeDeveloperToolsButton.click();
-        mainPage.findYourToolsButton.click();
+    public void Test2() {
+        //Test sorting price from high to low and validate numbers.
+        Login_func();
 
-        $("#products-page").shouldBe(visible);
+        WebElement Select = driver.findElement(By.className("product_sort_container"));
+        Select.click();
+        WebElement option = driver.findElement(By.xpath("//*[@id=\"header_container\"]/div[2]/div/span/select/option[4]"));
+        option.click();
+        //
+        WebElement inventoryContainer = driver.findElement(By.id("inventory_container"));
+        List<WebElement> inventoryItems = inventoryContainer.findElements(By.className("inventory_item"));
+        Double previousPrice = null;
+        for (WebElement item : inventoryItems) {
+            WebElement price = item.findElement(By.className("inventory_item_price"));
+            String priceText = price.getText().replace("$", "");
+            Double currentPrice = Double.parseDouble(priceText);
 
-        assertEquals("All Developer Tools and Products by JetBrains", Selenide.title());
+            if (previousPrice != null) {
+                assertTrue(currentPrice <= previousPrice, "Not list high to low");
+            }
+            previousPrice = currentPrice;
+        }
+    }
+
+    @Test
+    public void Test3() {
+        //add 2 products to the cart and proceed to complete check out items.
+        Login_func();
+        WebElement addBackPack = driver.findElement(By.id("add-to-cart-sauce-labs-backpack"));
+        addBackPack.click();
+        WebElement addBikeLight = driver.findElement(By.id("add-to-cart-sauce-labs-bike-light"));
+        addBikeLight.click();
+        WebElement cart = driver.findElement(By.className("shopping_cart_link"));
+        cart.click();
+        WebElement checkOut = driver.findElement(By.id("checkout"));
+        checkOut.click();
+        WebElement firstName = driver.findElement(By.id("first-name"));
+        WebElement lastName = driver.findElement(By.id("last-name"));
+        WebElement postal = driver.findElement(By.id("postal-code"));
+        firstName.sendKeys("Test_Name");
+        lastName.sendKeys("Test_LastName");
+        postal.sendKeys("1234");
+        WebElement continueButton = driver.findElement(By.id("continue"));
+        continueButton.click();
+        WebElement finish = driver.findElement(By.id("finish"));
+        finish.click();
+        WebElement complete = driver.findElement(By.className("complete-header"));
+        assertEquals("Thank you for your order!", complete.getText());
+    }
+
+    @Test
+    public void Test4() {
+        //check log check out summary page and check total price both before tax and after tax.
+        Login_func();
+        WebElement addBackPack = driver.findElement(By.id("add-to-cart-sauce-labs-backpack"));
+        addBackPack.click();
+        WebElement addBikeLight = driver.findElement(By.id("add-to-cart-sauce-labs-bike-light"));
+        addBikeLight.click();
+        WebElement cart = driver.findElement(By.className("shopping_cart_link"));
+        cart.click();
+        WebElement checkOut = driver.findElement(By.id("checkout"));
+        checkOut.click();
+        WebElement firstName = driver.findElement(By.id("first-name"));
+        WebElement lastName = driver.findElement(By.id("last-name"));
+        WebElement postal = driver.findElement(By.id("postal-code"));
+        firstName.sendKeys("Test_Name");
+        lastName.sendKeys("Test_LastName");
+        postal.sendKeys("1234");
+        WebElement continueButton = driver.findElement(By.id("continue"));
+        continueButton.click();
+
+        // Find the cart list container
+        WebElement cartList = driver.findElement(By.className("cart_list"));
+
+        // Find all cart items
+        List<WebElement> cartItems = cartList.findElements(By.className("cart_item"));
+
+        // Initialize total price variable
+        double totalPrice = 0.0;
+
+        // Iterate through each cart item to calculate total price
+        for (WebElement item : cartItems) {
+            // Find the price element for each item
+            WebElement priceElement = item.findElement(By.className("inventory_item_price"));
+            String priceText = priceElement.getText().replace("$", "");
+            double itemPrice = Double.parseDouble(priceText);
+
+            // Add item price to total price
+            totalPrice += itemPrice;
+        }
+        WebElement itemTotal = driver.findElement(By.className("summary_subtotal_label"));
+        String Total_before_tax = itemTotal.getText().replace("Item total: $", "");
+        double priceTotal = Double.parseDouble(Total_before_tax);
+        WebElement afterTaxPrice = driver.findElement(By.className("summary_total_label"));
+        String Total_after_tax = afterTaxPrice.getText().replace("Total: $", "");
+        double total = Double.parseDouble(Total_after_tax);
+        assertEquals(totalPrice, priceTotal );
+        double priceWithTax = totalPrice+(totalPrice*0.08);
+        DecimalFormat df = new DecimalFormat("#.##");
+        String formattedString = df.format(priceWithTax);
+        double expectedPrice = Double.parseDouble(formattedString);
+        assertEquals(expectedPrice,total);
+    }
+
+    @Test
+    public void Test5() {
+        //validate that Locked_out_user can not login to the system.
+        driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+        WebElement username = driver.findElement(By.id("user-name"));
+        WebElement password = driver.findElement(By.id("password"));
+        username.sendKeys("locked_out_user");
+        password.sendKeys("secret_sauce");
+        WebElement LoginButton = driver.findElement(By.id("login-button"));
+        LoginButton.click();
+        WebElement errorMsg = driver.findElement(By.className("error-message-container"));
+        assertEquals("Epic sadface: Sorry, this user has been locked out.",errorMsg.getText());
+    }
+
+    @AfterTest
+    public void teardown() {
+            driver.quit();
+
     }
 }
